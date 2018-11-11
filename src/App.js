52d8660f8a79bd25cbe604b2e3325464ea2f3a56
Gpx <polvara@gmail.com>
2018-11-11 22:11:02 +0100

    add labels

--- src/App.js
+++ src/App.js
@@ -1,113 +1,113 @@
-import React, { useState, useEffect } from 'react'
-import uuid from 'uuid/v4'
-import styled, { createGlobalStyle } from 'styled-components'
-import { throttle } from 'lodash'
-import db from './db'
-import Todos from './components/Todos'
-import TodoForm from './components/TodoForm'
-import BackgroundCourtesy from './components/BackgroundCourtesy'
+import React, { useState, useEffect } from "react";
+import uuid from "uuid/v4";
+import styled, { createGlobalStyle } from "styled-components";
+import { throttle } from "lodash";
+import db from "./db";
+import Todos from "./components/Todos";
+import TodoForm from "./components/TodoForm";
+import BackgroundCourtesy from "./components/BackgroundCourtesy";
 
 const UNSPLASH_KEY =
-  'd5fc56aca800600ffae7a2cd04be7ee6bb1a7baee5967b578c4ac55112b0aa19'
+  "d5fc56aca800600ffae7a2cd04be7ee6bb1a7baee5967b578c4ac55112b0aa19";
 
 const GlobalStyle = createGlobalStyle`
   body{
     min-height: 100vh;
-    background-color: ${p => p.color || '#455d7a'};
+    background-color: ${p => p.color || "#455d7a"};
     background-image: url(${p => p.background});
     background-size: cover;
     background-repeat: no-repeat;
     background-position: center;
     color: #fff;
   }
-`
+`;
 
 const Container = styled.div`
   width: 40vw;
   margin: auto;
   background: rgba(35, 49, 66, 0.6);
   padding: 16px 32px;
-`
+`;
 
 function App() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
   useEffect(() => {
     const unsubscribe = db
-      .collection('todos')
-      .where('deleted', '==', false)
-      .where('completed', '==', false)
-      .orderBy('createdAt', 'asc')
-      .onSnapshot(snapshot => setTodos(snapshot.docs))
-    return unsubscribe
-  }, [])
+      .collection("todos")
+      .where("deleted", "==", false)
+      .where("completed", "==", false)
+      .orderBy("createdAt", "asc")
+      .onSnapshot(snapshot => setTodos(snapshot.docs));
+    return unsubscribe;
+  }, []);
 
   function handleCreateTodo(text) {
-    db.collection('todos')
+    db.collection("todos")
       .doc(uuid())
-      .set({ text, completed: false, deleted: false, createdAt: new Date() })
+      .set({ text, completed: false, deleted: false, createdAt: new Date() });
   }
 
   function completeTodo(todo) {
-    db.collection('todos')
+    db.collection("todos")
       .doc(todo.id)
-      .update({ completed: true })
+      .update({ completed: true });
   }
 
   function deleteTodo(todo) {
-    db.collection('todos')
+    db.collection("todos")
       .doc(todo.id)
-      .update({ deleted: true })
+      .update({ deleted: true });
   }
 
   function handleUpdateText(todo, text) {
-    db.collection('todos')
+    db.collection("todos")
       .doc(todo.id)
-      .update({ text })
+      .update({ text });
   }
 
-  const [selectedIndex, setSelectedIndex] = useState(0)
+  const [selectedIndex, setSelectedIndex] = useState(0);
   useEffect(
     () => {
       const handleKeyPress = throttle(evt => {
-        if (evt.target !== document.body) return
+        if (evt.target !== document.body) return;
         switch (evt.key) {
-          case 'j':
-            setSelectedIndex(Math.min(selectedIndex + 1, todos.length - 1))
-            break
-          case 'k':
-            setSelectedIndex(Math.max(selectedIndex - 1, 0))
-            break
+          case "j":
+            setSelectedIndex(Math.min(selectedIndex + 1, todos.length - 1));
+            break;
+          case "k":
+            setSelectedIndex(Math.max(selectedIndex - 1, 0));
+            break;
           default:
         }
-      }, 300)
-      document.addEventListener('keydown', handleKeyPress)
+      }, 300);
+      document.addEventListener("keydown", handleKeyPress);
 
-      return () => document.removeEventListener('keydown', handleKeyPress)
+      return () => document.removeEventListener("keydown", handleKeyPress);
     },
     [selectedIndex, todos]
-  )
+  );
 
-  const [background, setBackground] = useState()
+  const [background, setBackground] = useState();
   useEffect(() => {
     async function fetchNewBackground() {
       const response = await fetch(
         `https://api.unsplash.com/photos/random?client_id=${UNSPLASH_KEY}&query=wallpapers&featured=true`
-      )
-      const photo = await response.json()
-      setBackground(photo)
+      );
+      const photo = await response.json();
+      setBackground(photo);
       localStorage.setItem(
-        'background',
+        "background",
         JSON.stringify({ date: Date.now(), photo })
-      )
+      );
     }
 
-    const storedBackground = localStorage.getItem('background')
+    const storedBackground = localStorage.getItem("background");
     if (storedBackground) {
-      const { date, photo } = JSON.parse(storedBackground)
-      if (Date.now() > date + 24 * 60 * 60 * 1000) fetchNewBackground()
-      else setBackground(photo)
-    } else fetchNewBackground()
-  }, [])
+      const { date, photo } = JSON.parse(storedBackground);
+      if (Date.now() > date + 24 * 60 * 60 * 1000) fetchNewBackground();
+      else setBackground(photo);
+    } else fetchNewBackground();
+  }, []);
 
   return (
     <Container>
@@ -126,7 +126,7 @@ function App() {
       />
       <TodoForm onCreateTodo={handleCreateTodo} />
     </Container>
-  )
+  );
 }
 
-export default App
+export default App;
